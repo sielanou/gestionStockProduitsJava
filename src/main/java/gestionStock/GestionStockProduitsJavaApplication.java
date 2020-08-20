15bb1e9c1d3cec6c41038cf7d2bc79965e5b6b5e
monthe <vous@example.com>
2020-08-20 18:53:36 +0200

    first commit

---
+++ src/main/java/gestionStock/GestionStockProduitsJavaApplication.java
@@ -0,0 +1,13 @@
+package gestionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockProduitsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockProduitsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestionStock/GestionStockProduitsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package gestionStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockProduitsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
